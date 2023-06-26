e2c60f2fe567a5b718c8e05d76450c4ff2aa920d
P Sathya Narayan <sathyanmbpk@gmail.com>
2023-06-26 23:30:12 +0530

    2nd Commit

--- main.py
+++ main.py
@@ -1,27 +1,76 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from starlette.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from typing import Dict
+import pytesseract
+import io
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from PIL import Image
+from googletrans import Translator
+
+
+# Set the path to the Tesseract executable
+pytesseract.pytesseract.tesseract_cmd = '/usr/local/bin/tesseract'
 
 app = FastAPI()
 
-class Msg(BaseModel):
-    msg: str
+# Mount the static files directory
+app.mount("/static", StaticFiles(directory="static"), name="static")
+translator = Translator(service_urls=['translate.google.com'])
+
+@app.get('/')
+async def home():
+    # Return the static HTML file as the home page
+    return FileResponse('static/index.html')
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World. Welcome to FastAPI!"}
+class OCRResponse(BaseModel):
+    text: str
 
 
-@app.get("/path")
-async def demo_get():
-    return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
+class TranslationRequest(BaseModel):
+    text: str
 
 
-@app.post("/path")
-async def demo_post(inp: Msg):
-    return {"message": inp.msg.upper()}
+class TranslationResponse(BaseModel):
+    translation: str
 
 
-@app.get("/path/{path_id}")
-async def demo_get_path_id(path_id: int):
-    return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
+
+@app.post("/translate/malayalam-to-english")
+async def translate_malayalam_to_english(request: TranslationRequest):
+    text = request.text
+    translator = Translator()
+    translation = translator.translate(text, src="ml", dest="en")
+    x = TranslationResponse(translation=translation.text)
+    print(TranslationResponse(translation=translation.text))
+    return x
+@app.post('/ocr/malayalam')
+async def ocr_malayalam(file: UploadFile = File(...)) -> Dict[str, str]:
+    # Check if a file was uploaded
+    if not file:
+        return {'error': 'No file uploaded'}
+    
+    # Check if the file is an image
+    if not file.content_type.startswith('image/'):
+        return {'error': 'File must be an image'}
+    
+    # Read the image
+    img = Image.open(io.BytesIO(await file.read()))
+    
+    # Perform OCR on the image with Malayalam language
+    text = pytesseract.image_to_string(img, lang='mal')
+    
+    # Return the OCR result as JSON
+    return {'text': text}
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host='localhost', port=8000)
